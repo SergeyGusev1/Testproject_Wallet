b27fc7bb244e57c6e7c59b1ec704a438a10bba0c
Сергей Гусев <magnatuch@vk.com>
2026-02-21 14:31:04 +0300

    Исправления и requirements

--- app/core/config.py
+++ app/core/config.py
@@ -3,7 +3,7 @@ from pydantic import BaseSettings
 
 class Settings(BaseSettings):
     app_title: str = 'Кошелёк'
-    database_url: str = 'sqlite+aiosqlite:///./test.db''
+    database_url: str = 'sqlite+aiosqlite:///./test.db'
 
     class Config:
         env_file = '.env'
